61a5e06b0a846ae57fc3bdb5f35d6cc1c976d2e4
Jon Robison <narfman0@gmail.com>
2019-10-22 15:48:45 -0400

    -Move that hello world text all around

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,26 @@
 use cgmath;
 use ggez;
 
-use ggez::event;
-use ggez::graphics;
-use ggez::{Context, GameResult};
-use std::env;
-use std::path;
+use ggez::event::{KeyCode, KeyMods};
+use ggez::{event, graphics, Context, GameResult};
+use std::{env, path};
+use cgmath::{Point2, Vector2};
 
 // First we make a structure to contain the game's state
 struct MainState {
     frames: usize,
+    pos: Point2<i32>,
+    vel: Vector2<i32>,
     text: graphics::Text,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        // The ttf file will be in your resources directory. Later, we
-        // will mount that directory so we can omit it in the path here.
         let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf")?;
         let text = graphics::Text::new(("Hello world!", font, 48.0));
-
-        let s = MainState { frames: 0, text };
+        let pos = Point2::new(0, 0);
+        let vel = Vector2::new(0, 0);
+        let s = MainState { frames: 0, pos, vel, text };
         Ok(s)
     }
 }
@@ -34,38 +34,34 @@ impl MainState {
 // that you can override if you wish, but the defaults are fine.
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos += self.vel;
+        self.frames += 1;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
-
-        // Drawables are drawn from their top-left corner.
-        let offset = self.frames as f32 / 10.0;
-        let dest_point = cgmath::Point2::new(offset, offset);
+        let dest_point = cgmath::Point2::new(self.pos.x as f32, self.pos.y as f32);
         graphics::draw(ctx, &self.text, (dest_point,))?;
         graphics::present(ctx)?;
-
-        self.frames += 1;
         if (self.frames % 100) == 0 {
             println!("FPS: {}", ggez::timer::fps(ctx));
         }
 
         Ok(())
     }
+
+    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods, _repeat: bool) {
+        match keycode {
+            KeyCode::Up | KeyCode::W => self.vel.y += 10,
+            KeyCode::Left | KeyCode::A => self.vel.x -= 1,
+            KeyCode::Right | KeyCode::D => self.vel.x += 1,
+            _ => (),
+        }
+    }
 }
 
-// Now our main function, which does three things:
-//
-// * First, create a new `ggez::ContextBuilder`
-// object which contains configuration info on things such
-// as screen resolution and window title.
-// * Second, create a `ggez::game::Game` object which will
-// do the work of creating our MainState and running our game.
-// * Then, just call `game.run()` which runs the `Game` mainloop.
 pub fn main() -> GameResult {
-    // We add the CARGO_MANIFEST_DIR/resources to the resource paths
-    // so that ggez will look in our cargo project directory for files.
     let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
         let mut path = path::PathBuf::from(manifest_dir);
         path.push("resources");
@@ -76,7 +72,6 @@ pub fn main() -> GameResult {
 
     let cb = ggez::ContextBuilder::new("helloworld", "ggez").add_resource_path(resource_dir);
     let (ctx, event_loop) = &mut cb.build()?;
-
     let state = &mut MainState::new(ctx)?;
     event::run(ctx, event_loop, state)
 }
