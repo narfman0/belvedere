fb9829356a4d70b64b43b25f7ecb899b7370f87f
Jon Robison <narfman0@gmail.com>
2019-10-23 12:19:25 -0400

    -Draw a magical reg png instead of a boring letter hyyyype

--- src/states.rs
+++ src/states.rs
@@ -5,11 +5,14 @@ use ggez::event::{KeyCode, KeyMods};
 use ggez::{event, graphics, Context, GameResult};
 use cgmath::{Point2, Vector2};
 
+static PLAYER_WIDTH: i32 = 4;
+
 pub struct MainState {
     frames: usize,
     pos: Point2<i32>,
     vel: Vector2<i32>,
     text: graphics::Text,
+    player_image: graphics::Image,
 }
 
 impl MainState {
@@ -18,7 +21,8 @@ impl MainState {
         let text = graphics::Text::new(("A", font, 48.0));
         let pos = Point2::new(0, 0);
         let vel = Vector2::new(0, 0);
-        let s = MainState { frames: 0, pos, vel, text };
+        let player_image = graphics::Image::new(ctx, "/red.png")?;
+        let s = MainState { frames: 0, pos, vel, text, player_image };
         Ok(s)
     }
 }
@@ -31,20 +35,23 @@ impl event::EventHandler for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let r = ggez::graphics::screen_coordinates(ctx);
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
-        let dest_point = cgmath::Point2::new(self.pos.x as f32, self.pos.y as f32);
-        graphics::draw(ctx, &self.text, (dest_point,))?;
+        //let dest_point = cgmath::Point2::new(self.pos.x as f32, r.h - self.pos.y as f32);
+        //graphics::draw(ctx, &self.text, (dest_point,))?;
+        let player_pos = cgmath::Point2::new((self.pos.x - PLAYER_WIDTH/2) as f32, r.h - (self.pos.y - PLAYER_WIDTH/2) as f32);
+        graphics::draw(ctx, &self.player_image, (player_pos,))?;
         graphics::present(ctx)?;
         if (self.frames % 100) == 0 {
             println!("FPS: {}", ggez::timer::fps(ctx));
         }
-
         Ok(())
     }
 
     fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods, _repeat: bool) {
         match keycode {
-            KeyCode::Up | KeyCode::W => self.vel.y += 10,
+            KeyCode::Up | KeyCode::W => self.vel.y += 1,
+            KeyCode::Down | KeyCode::S => self.vel.y -= 1,
             KeyCode::Left | KeyCode::A => self.vel.x -= 1,
             KeyCode::Right | KeyCode::D => self.vel.x += 1,
             _ => (),
