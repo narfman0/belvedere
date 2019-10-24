1026a612206e2fed7da7099482a4b959a99d5dba
Jon Robison <narfman0@gmail.com>
2019-10-24 14:49:10 -0400

    -Swap to floats -Abstract input slightly

--- src/states.rs
+++ src/states.rs
@@ -9,23 +9,39 @@ static PLAYER_WIDTH: i32 = 4;
 
 pub struct MainState {
     frames: usize,
-    pos: Point2<i32>,
-    vel: Vector2<i32>,
+    pos: Point2<f32>,
+    vel: Vector2<f32>,
     player_image: graphics::Image,
+    left: bool,
+    right: bool,
+    up: bool,
+    down: bool,
 }
 
 impl MainState {
     pub fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let pos = Point2::new(0, 0);
-        let vel = Vector2::new(0, 0);
+        let pos = Point2::new(0.0, 0.0);
+        let vel = Vector2::new(0.0, 0.0);
         let player_image = graphics::Image::new(ctx, "/red.png")?;
-        let s = MainState { frames: 0, pos, vel, player_image };
+        let s = MainState { frames: 0, pos, vel, player_image, up: false, down: false, left: false, right: false };
         Ok(s)
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        if self.up {
+            self.vel.y += 0.1;
+        }
+        if self.down {
+            self.vel.y -= 0.1;
+        }
+        if self.left {
+            self.vel.x -= 0.1;
+        }
+        if self.right {
+            self.vel.x += 0.1;
+        }
         self.pos += self.vel;
         self.frames += 1;
         Ok(())
@@ -34,7 +50,7 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let r = ggez::graphics::screen_coordinates(ctx);
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
-        let player_pos = cgmath::Point2::new((self.pos.x - PLAYER_WIDTH/2) as f32, r.h - (self.pos.y - PLAYER_WIDTH/2) as f32);
+        let player_pos = cgmath::Point2::new(self.pos.x - PLAYER_WIDTH as f32/2.0, r.h - (self.pos.y - PLAYER_WIDTH as f32/2.0));
         graphics::draw(ctx, &self.player_image, (player_pos,))?;
         graphics::present(ctx)?;
         if (self.frames % 100) == 0 {
@@ -45,10 +61,20 @@ impl event::EventHandler for MainState {
 
     fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods, _repeat: bool) {
         match keycode {
-            KeyCode::Up | KeyCode::W => self.vel.y += 1,
-            KeyCode::Down | KeyCode::S => self.vel.y -= 1,
-            KeyCode::Left | KeyCode::A => self.vel.x -= 1,
-            KeyCode::Right | KeyCode::D => self.vel.x += 1,
+            KeyCode::Up | KeyCode::W => self.up = true,
+            KeyCode::Down | KeyCode::S => self.down = true,
+            KeyCode::Left | KeyCode::A => self.left = true,
+            KeyCode::Right | KeyCode::D => self.right = true,
+            _ => (),
+        }
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods) {
+        match keycode {
+            KeyCode::Up | KeyCode::W => self.up = false,
+            KeyCode::Down | KeyCode::S => self.down = false,
+            KeyCode::Left | KeyCode::A => self.left = false,
+            KeyCode::Right | KeyCode::D => self.right = false,
             _ => (),
         }
     }
